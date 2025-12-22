c54a039eab3b574a719c8a6d9064bb38d26d90d7
Sccrap <102134977+Sccrap@users.noreply.github.com>
2025-12-22 17:00:34 +0300

    Delete main_cli.go

--- main_cli.go
+++
@@ -1,9 +0,0 @@
-// +build !gui
-
-package main
-
-// Placeholder для CLI-only версии
-// GUI функция недоступна в режиме CLI
-func RunGUI() {
-	println("GUI not available in CLI build. Use analyzer without --gui flag")
-}
